661c87a78a2f9a596e01e21c3647de9a1f37bc9e
anastasia <anastasia.ethno@gmail.com>
2023-07-23 12:08:52 +0200

    feat: add valodators solution

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,8 @@
+import { filter, equals, prop, values, compose, length, countBy, identity, allPass, anyPass, gte } from 'ramda';
+
+const isColor = color => propColor => compose(equals(color), prop(propColor));
+const countColor = color => compose(length, filter(equals(color)), values);
+
 /**
  * @file Домашка по FP ч. 1
  *
@@ -14,37 +19,58 @@
  */
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
-
-    return star === 'red' && square === 'green';
-};
+export const validateFieldN1 = obj => allPass([
+    isColor('red')('star'),
+    isColor('green')('square'),
+    () => equals(2, countColor('white')(obj))
+])(obj);
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = obj => gte(countColor('green')(obj), 2);
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = obj => equals(countColor('red')(obj), countColor('blue')(obj));
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = allPass([
+    isColor('red')('star'),
+    isColor('orange')('square'),
+    isColor('blue')('circle')
+]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = obj => {
+    const counts = countBy(identity, values(obj));
+    const predicates = Object.keys(counts).map(color => {
+        if (color !== 'white' && counts[color] >= 3) {
+            return () => true;
+        }
+        return () => false;
+    });
+    return anyPass(predicates)(obj);
+};
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = obj => allPass([
+    () => equals(2, countColor('green')(obj)),
+    () => equals(1, countColor('red')(obj)),
+    isColor('green')('triangle')
+])(obj);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = compose(equals(4), countColor('orange'));
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = obj => {
+    const starColor = prop('star', obj);
+    return !equals(starColor, 'red') && !equals(starColor, 'white');
+};
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = compose(equals(4), countColor('green'));
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = allPass([
+    obj => equals(prop('triangle', obj), prop('square', obj)),
+    obj => !equals('white', prop('triangle', obj))
+]);
