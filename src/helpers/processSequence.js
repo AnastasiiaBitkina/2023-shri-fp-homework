f69416d1d80bfbf784670a85f84987dca887faec
anastasia <anastasia.ethno@gmail.com>
2023-07-23 21:38:08 +0200

    feat: add sequence solution

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,60 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+    __, allPass, andThen, assoc, compose, concat, gt, ifElse, length, lt, mathMod, otherwise, 
+    partial, prop, tap, test
+} from "ramda";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const squareNum = x => x ** 2;
+const checkGreaterThanTwo = gt(__, 2);
+const checkLessThanTen = lt(__, 10);
+const convertStringToNumber = compose(Math.round, Number);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const validateLengthMoreThanTwo = compose(checkGreaterThanTwo, length);
+const validateLengthLessThanTen = compose(checkLessThanTen, length);
+const isNumericString = test(/^[0-9]+\.?[0-9]+$/);
+const modByThreeAndToString = compose(String, mathMod(__, 3));
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const BASE_NUM_URL = 'https://api.tech/numbers/base';
+const ANIMALS_API_URL = 'https://animals.tech/';
+const extractResult = compose(String, prop('result'));
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const assocToBinary = assoc('number', __, { from: 10, to: 2 });
+const getBinaryFromApi = compose(api.get(BASE_NUM_URL), assocToBinary);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const isValid = allPass([validateLengthMoreThanTwo, validateLengthLessThanTen, isNumericString]);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const executeSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+    const logAction = tap(writeLog);
+    const onSuccess = andThen(handleSuccess);
+    const onError = otherwise(handleError);
 
-export default processSequence;
+    const validationError = partial(handleError, ['ValidationError']);
+
+    const coreSequence = compose(
+        onError,
+        onSuccess,
+        andThen(extractResult),
+        andThen(api.get(__, {})),
+        andThen(concat(ANIMALS_API_URL)),
+        andThen(modByThreeAndToString),
+        andThen(squareNum),
+        andThen(length),
+        andThen(extractResult),
+        getBinaryFromApi,
+        logAction,
+        convertStringToNumber
+    );
+
+    const conditionalRun = ifElse(isValid, coreSequence, validationError);
+    const startSequence = compose(conditionalRun, logAction);
+
+    startSequence(value);
+};
+
+export default executeSequence;
+
